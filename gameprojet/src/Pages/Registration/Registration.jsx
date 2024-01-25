97e38ee5e635c6b9c664bcff4eb19c6a9c910a1e
Akbar <iron.man96321@gmail.com>
2024-01-25 21:38:17 +0600

    F 1 COMM

---
+++ gameprojet/src/Pages/Registration/Registration.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Registration = () => {
+    return (
+        <div>
+            sffssdfgsfsdandsklghdsuign;jijgo;dn
+        </div>
+    );
+};
+
+export default Registration;
